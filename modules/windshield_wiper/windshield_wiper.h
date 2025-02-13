4ca1c0c21ae8da79404389593d4d7af236ac3d9e
BaileyClark33 <91906664+BaileyClark33@users.noreply.github.com>
2025-02-12 20:26:02 -0500

    Update windshield_wiper.h

--- modules/windshield_wiper/windshield_wiper.h
+++ modules/windshield_wiper/windshield_wiper.h
@@ -7,12 +7,7 @@
 
 //=====[Declaration of public data types]======================================
 
-typedef enum {
-  WIPERS_OFF,
-  WIPERS_INT,
-  WIPERS_HI,
-  WIPERS_LOW
-} windshield_state_t;
+
 
 //=====[Declarations (prototypes) of public functions]=========================
 
